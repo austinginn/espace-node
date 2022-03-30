eb81e8943f3c9462dd3776bc6aaa44735236c704
root <root@DESKTOP-K9F4EKE.localdomain>
2022-03-29 23:00:21 -0400

    narrowed to just getEventList + ES6

--- test.js
+++ test.js
@@ -1,40 +1,10 @@
 import Espace from "./espace-node.mjs";
-import fetch from "node-fetch";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+let es = new Espace(process.env.USERNAME, process.env.SECRET);
+let events = await es.getEventList();
+console.log(events);
 
 
-
-
-
-console.log(Espace);
-
-
-// let es = Espace("aginn@afumc.org", "uRWTMehMG$4dcEU","AFUMC");
-
-
-
-// console.log(es);
-
-
-
-//Fetch GET request wrapper and error handling
-const get = async url => {
-    try {
-        const response = await fetch(url, {
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': 'Basic YWdpbm5AYWZ1bWMub3JnOnVSV1RNZWhNRyQ0ZGNFVQ=='
-            },
-        });
-        // const status = checkStatus(await response.status());
-        const data = await response.json();
-        // console.log(status);
-        // console.log(data);
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
-}
-
-let result = await get("https://api.espace.cool/api/v1/event/list?nextDays=10000&publicOnly=true");
-
- console.log(result.Data.length);
